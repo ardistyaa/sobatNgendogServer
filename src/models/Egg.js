263b5beb3b77c5baf9257bd6a80f60d04c8df693
ardistya <ardistyaaa@gmail.com>
2025-05-23 19:04:09 +0700

    Add Egg Backend Service

---
+++ src/models/Egg.js
@@ -0,0 +1,25 @@
+import dbPool from "../config/database";
+
+class Egg{
+
+    static async findEggPrice() {
+        try{
+            const [rows] = await dbPool.query(`SELECT * FROM egg_price`)
+            return rows;
+        } catch (error) {
+            throw new Error(`Error fethcing all egg price : ${error.message}`);
+        }
+    } 
+
+    static async createEggPrice(eggData) {
+        const { price , date } = eggData;
+
+        try {
+            const [result] = await dbPool.query(`INSERT INTO egg_price (price, date) VALUES (?, ?)`, [price, date]);
+            
+            return result
+        } catch (error) {
+            throw error;
+        }
+    }
+}
